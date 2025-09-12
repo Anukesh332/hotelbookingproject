e4321cdf33d0ea3e1d3232c333f413ec7bbe4a64
Anukesh Sonawane <yogitapandey2305@gmail.com>
2025-09-12 18:43:06 +0530

    Done with project

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const connectDB = require('./src/config/db');
 
 const adminRoutes = require('./src/routes/admin');
 const searchRoutes = require('./src/routes/search');
+const errorHandler = require('./src/middleware/errorhandler');
 
 const app = express();
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json({ limit: '5mb' }));
 
-
+app.use(errorHandler);
 app.use('/api/admin', adminRoutes);
 app.use('/api', searchRoutes);
 
